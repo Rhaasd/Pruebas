e8653833f2dec4bf65333cd4c661fdd26a020a59
dam <dam@172.26.220.239>
2018-12-11 10:05:27 +0100

    Cuarta Versión

--- src/controlversiones/ControlVersiones.java
+++ src/controlversiones/ControlVersiones.java
@@ -7,15 +7,7 @@ package controlversiones;
 public class ControlVersiones {
 
     public static void main(String[] args) {
-        System.out.print("HolayADios");
-
-        System.out.print("AdiosSolo");
-        System.out.print("AdiosSolo2");
-        
-        //VERSION TERCERA
-
-        int nueva = 3555;
-
+        System.out.print("Versión última");
     }
 
 }
