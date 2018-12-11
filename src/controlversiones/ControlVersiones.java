9f22dad500e97e865570a9042274a3ea7a831e22
dam <dam@172.26.220.239>
2018-12-11 09:52:45 +0100

    Tercera Versión

--- src/controlversiones/ControlVersiones.java
+++ src/controlversiones/ControlVersiones.java
@@ -7,8 +7,15 @@ package controlversiones;
 public class ControlVersiones {
 
     public static void main(String[] args) {
-        System.out.print("Hola2");
+        System.out.print("HolayADios");
+
+        System.out.print("AdiosSolo");
+        System.out.print("AdiosSolo2");
         
+        //VERSION TERCERA
+
+        int nueva = 3555;
+
     }
 
 }
